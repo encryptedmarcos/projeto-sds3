ca9470699d3f96f57de111012d37f2ab7fbe628e
Marcos Pimentel <marcospimentelneto@outlook.com>
2021-09-11 16:38:23 -0300

    Project created

---
+++ backend/src/main/java/com/portifolio/springreactvendas/SpringreactvendasApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.springreactvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactvendasApplication.class, args);
+	}
+
+}
