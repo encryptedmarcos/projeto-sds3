50cc2b6cc06a4d39ce6382b235d693bb6cb77139
Marcos Pimentel <marcospimentelneto@outlook.com>
2021-09-12 23:07:35 -0300

    BarChart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,12 +24,6 @@ const DonutChart = () => {
             });
     }, []);
 
-    //dado estático
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-
     const options = {
         legend: {
             show: true
